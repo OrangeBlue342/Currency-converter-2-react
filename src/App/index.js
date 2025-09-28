d8c5fb09ecb2afc9ad571c2e049474e147483ee6
Joanna Czub <jk.czub@student.uw.edu.pl>
2025-09-28 19:34:55 +0200

    Currency-converter created

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './index.css';
+import { Form } from './Form';
+import { Clock } from './Clock';
+
+function App() {
+    return (
+        <div className="app">
+            <Clock />
+            <Form />
+        </div>
+    );
+}
+
+export default App;
