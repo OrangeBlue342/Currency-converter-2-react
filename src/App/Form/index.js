cad92cda149ecc978c6eaa637d4489cb681d492a
Joanna Czub <jk.czub@student.uw.edu.pl>
2025-10-12 20:24:09 +0200

    useRatesData added

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,4 +1,4 @@
- import React, { useState } from "react";
+import React, { useState } from "react";
 import { currencies } from "../currencies";
 import {Clock} from  "./Clock";
 import { Result } from "./Result";
